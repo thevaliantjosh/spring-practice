1b5466d4bb5f8d30f2511db4c5a25d06fb3eec3a
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-22 10:27:30 -0600

    Home Controller and index.html

---
+++ src/main/java/com/codeup/springpractice/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.codeup.springpractice.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String homePage(){
+        return "index";
+    }
+
+    @GetMapping("/all-parks")
+    public String parksPage(){
+        return "/all-parks";
+    }
+
+}
